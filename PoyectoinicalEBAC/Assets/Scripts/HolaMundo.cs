df49bfed1406c708a48e915ff2ffebef3b1699fa
Jose Castro <jacrzot7@gmail.com>
2025-05-03 00:31:15 -0600

    Tarea Modulo 5 EBAC
    -Modificacion de el script HOLA MUNDO imprimiendo en consola desde que funcion se esta llamando
    -Scripts para entender el comportamiento de condicion de carrera
    -Componente 1, componente 2, componente 3

--- PoyectoinicalEBAC/Assets/Scripts/HolaMundo.cs
+++ PoyectoinicalEBAC/Assets/Scripts/HolaMundo.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+    int x;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        x = 0;
         print("algo paso");
         Debug.Log("Algo paso");
         Debug.LogWarning("Algo salio medianamente mal");
@@ -14,6 +16,29 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //x = x + 1;
+        //Debug.Log(x);
+
+        Debug.Log("Hola desde Update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde Fixed Update cada 50 frames");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hola desde Late Update");
+    }
+
+    private void OnEnable()
+    {
+        Debug.LogWarning("el objeto ha sido habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("el objeto ha sido inhabilitado");
     }
 }
